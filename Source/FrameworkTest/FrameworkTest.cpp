628893ede87ac930f2773c7476cb643cf0b5f97a
Hilmi Barış Taşdemir <hbtsdmr@gmail.com>
2022-10-11 14:48:22 +0300

    Initial

---
+++ Source/FrameworkTest/FrameworkTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FrameworkTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FrameworkTest, "FrameworkTest" );
---
+++ Source/FrameworkTest/FrameworkTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FrameworkTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRAMEWORKTEST_API AFrameworkTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
